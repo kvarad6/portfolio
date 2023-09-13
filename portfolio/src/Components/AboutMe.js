41b445c149565c97a97f442159052416f43aa172
kvarad6 <kvarad6@gmail.com>
2023-09-14 01:55:54 +0530

    updated logo

--- portfolio/src/Components/AboutMe.js
+++ portfolio/src/Components/AboutMe.js
@@ -7,18 +7,19 @@ const AboutMe = () => {
     <>
         <Grid sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <Grid Item>
-                <Typography sx={{fontSize: 40 }}>About Me</Typography>
+                <Typography sx={{ textAlign: 'center', fontSize: 40 }}>About Me</Typography>
             </Grid>
             <Grid Item>
-                  <HorizontalRuleRoundedIcon fontSize='large' />
+                <HorizontalRuleRoundedIcon />
             </Grid>
             <Grid Item>
-                  <Typography variant="subtitle1" sx={{ textAlign: 'center', ml: 40, mr: 40, mt: 4 }}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+                <Typography variant="subtitle1" sx={{ textAlign: 'center', ml: 40, mr: 40, mt: 4 }}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
                     blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
                     neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
                     quasi quidem quibusdam.</Typography>
             </Grid>
         </Grid>
+        
     </>
   )
 }
--- portfolio/src/Components/Header.js
+++ portfolio/src/Components/Header.js
@@ -1,20 +1,23 @@
 import React from 'react'
 import { AppBar, Toolbar, Typography, Tabs, Tab } from '@mui/material'
 // import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';  
-
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 
 const Header = () => {
   const pages = ["Home", "About", "Resume", "Projects", "Contact"]
- 
+
   return (
     <>
       <AppBar elevation={0} position='fixed' sx={{ backgroundColor: '#2f6163', boxShadow: 'none' }}>
         <Toolbar>
-          <Typography sx={{ color: '#40c7cf', fontWeight: 500, fontSize: 20}}>Varad Kulkarni</Typography>
-          <Tabs sx={{ marginLeft: 'auto'}}indicatorColor='secondary'>
+          <KeyboardArrowLeftIcon />
+          <Typography sx={{ color: '#40c7cf', fontWeight: 500, fontSize: 20 }}>Varad Kulkarni <span style={{ color: 'white' }}>/</span></Typography>
+          <KeyboardArrowRightIcon />
+          <Tabs sx={{ marginLeft: 'auto' }} indicatorColor='secondary'>
             {
               pages.map((page, index) => (
-                <Tab sx={{ color:'#40c7cf'}} key={index} label={page}></Tab>
+                <Tab sx={{ color: '#40c7cf' }} key={index} label={page}></Tab>
               ))
             }
           </Tabs>
