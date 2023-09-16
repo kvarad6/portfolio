b350f1c4af9d9e123bf654d4bbe2a64b31d92283
kvarad6 <kvarad6@gmail.com>
2023-09-17 01:52:33 +0530

    updated timeline of about us and project section

--- portfolio/src/Components/AboutMe.js
+++ portfolio/src/Components/AboutMe.js
@@ -1,7 +1,19 @@
 import { Typography, Grid } from '@mui/material'
 import React from 'react'
 import HorizontalRuleRoundedIcon from '@mui/icons-material/HorizontalRuleRounded';
-
+import Avatar from '@mui/material/Avatar';
+import coderLogo from '../static/images/timeline/coderLogo.png'
+import photography from '../static/images/timeline/photography.png'
+import milestone from '../static/images/timeline/milestone.png'
+import handshake from '../static/images/timeline/handshake.png'
+import sports from '../static/images/timeline/sports.png'
+import hi from '../static/images/timeline/hi.png'
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
 
 const AboutMe = () => {
     return (
@@ -14,7 +26,7 @@ const AboutMe = () => {
                     <HorizontalRuleRoundedIcon fontSize='large' />
                 </Grid>
             </Grid>
-            <Grid sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: 5, ml: 45, mr: 45, textAlign: 'justify' }}>
+            {/* <Grid sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: 5, ml: 45, mr: 45, textAlign: 'justify' }}>
                 <Grid Item xs={4}>
                     <Typography variant="subtitle1" sx={{ color: '#3AA6B9'}}>
                         Hey there! Thanks for stopping by my little corner of the internet. I'm Varad Kulkarni, a passionate software engineer who has been riding the IT rollercoaster for a thrilling year now. 🖥️ My journey in the world of technology has been nothing short of exhilarating, and I'm here to share a glimpse of my story with you.</Typography>
@@ -39,7 +51,81 @@ const AboutMe = () => {
                     <Typography variant="subtitle1" sx={{ color: '#3AA6B9' }}>
                         🌐 Let's Connect: If you share my passion for technology, the thrill of sports, or the art of storytelling through photography, let's connect and embark on new adventures together. Whether it's collaborating on a coding project, hitting the sports field, or exploring the world through a camera lens, I'm always up for the next great adventure. </Typography>
                 </Grid>
-            </Grid>
+            </Grid> */}
+            <Timeline position="alternate">
+                <TimelineItem>
+                    <TimelineSeparator>
+                        <TimelineDot>
+                            <Avatar src={hi} />
+                        </TimelineDot>
+                        <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent sx={{ mb:4 }}>
+                        <Typography variant="subtitle1" sx={{ color: '#3AA6B9' }}>
+                            Hey there! Thanks for stopping by my little corner of the internet. I'm Varad Kulkarni, a passionate software engineer who has been riding the IT rollercoaster for a thrilling year now. 🖥️ My journey in the world of technology has been nothing short of exhilarating, and I'm here to share a glimpse of my story with you.</Typography>
+                    </TimelineContent>
+                </TimelineItem>
+                <TimelineItem>
+                    <TimelineSeparator>
+                        <TimelineDot>
+                            <Avatar src={coderLogo} />
+                        </TimelineDot>
+                        <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent sx={{ mb: 4 }}>
+                        <Typography variant="subtitle1" sx={{ color: '#3AA6B9' }}>
+                            Code Whisperer: In the realm of ones and zeros, I'm the code whisperer, crafting digital wonders and solving real-world puzzles. Whether it's crafting elegant algorithms or building robust applications, I thrive on the thrill of creating software that makes lives easier and more efficient. 💻</Typography>
+                    </TimelineContent>
+                </TimelineItem>
+                <TimelineItem>
+                    <TimelineSeparator>
+                        <TimelineDot>
+                            <Avatar src={sports} />
+                        </TimelineDot>
+                        <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent sx={{ mb: 4 }}>
+                        <Typography variant="subtitle1" sx={{ color: '#3AA6B9' }}>
+                            Sports Enthusiast: When I'm not immersed in lines of code, you'll likely find me on the field, court, or track, chasing that adrenaline rush. Sports are my sanctuary, where I push my limits, foster teamwork, and savor the sweet taste of victory. From basketball's swish to the thrill of a finish line, I'm always up for the challenge. 🏆 </Typography>
+                    </TimelineContent>
+                </TimelineItem>
+                <TimelineItem>
+                    <TimelineSeparator>
+                        <TimelineDot>
+                            <Avatar src={photography} />
+                        </TimelineDot>
+                        <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent sx={{ mb: 4 }}>
+                        <Typography variant="subtitle1" sx={{ color: '#3AA6B9' }}>
+                            Capturing Moments: Beyond the digital world, I have a deep appreciation for capturing life's precious moments through the lens. Photography is my way of freezing time and sharing stories through visuals. Whether it's a breathtaking sunset or candid shots of friends, I believe every picture has a unique tale to tell. 📸</Typography>
+                    </TimelineContent>
+                </TimelineItem>
+                <TimelineItem>
+                    <TimelineSeparator>
+                        <TimelineDot>
+                            <Avatar src={milestone} />
+                        </TimelineDot>
+                        <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent sx={{ mb: 4 }}>
+                        <Typography variant="subtitle1" sx={{ color: '#3AA6B9' }}>
+                            Celebrating Milestones:As I celebrate my first year in the IT industry, I can't help but feel grateful for the incredible opportunities, the fantastic people I've met, and the growth I've experienced. The future holds limitless possibilities, and I'm excited to continue this amazing journey, turning code into innovation and dreams into reality. </Typography>
+                    </TimelineContent>
+                </TimelineItem>
+                <TimelineItem>
+                    <TimelineSeparator>
+                        <TimelineDot>
+                            <Avatar src={handshake} />
+                        </TimelineDot>
+                        <TimelineConnector />
+                    </TimelineSeparator>
+                    <TimelineContent sx={{ mb: 4 }}>
+                        <Typography variant="subtitle1" sx={{ color: '#3AA6B9' }}>
+                            Let's Connect: If you share my passion for technology, the thrill of sports, or the art of storytelling through photography, let's connect and embark on new adventures together. Whether it's collaborating on a coding project, hitting the sports field, or exploring the world through a camera lens, I'm always up for the next great adventure. </Typography>
+                    </TimelineContent>
+                </TimelineItem>
+            </Timeline>
         </>
     )
 }
--- portfolio/src/Components/Resume.js
+++ portfolio/src/Components/Resume.js
@@ -1,19 +1,22 @@
 import React from 'react'
-import { Typography, Grid, ListItemButton } from '@mui/material'
+import { Typography, Grid } from '@mui/material'
 import HorizontalRuleRoundedIcon from '@mui/icons-material/HorizontalRuleRounded';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Divider from '@mui/material/Divider';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
-import tejgyanLogo from '../static/images/avatar/tejgyanLogo.png'
-import searceLogoBlack from '../static/images/avatar/searceLogoBlack.png'
-import viitLogo from '../static/images/avatar/viitLogo.png'
-import narayanaLogoBlack from '../static/images/avatar/narayanaLogoBlack.png'
-import jnvLogo from '../static/images/avatar/jnvLogo.png'
+// import Avatar from '@mui/material/Avatar';
+// import tejgyanLogo from '../static/images/avatar/tejgyanLogo.png'
+// import searceLogoBlack from '../static/images/avatar/searceLogoBlack.png'
+// import viitLogo from '../static/images/avatar/viitLogo.png'
+// import narayanaLogoBlack from '../static/images/avatar/narayanaLogoBlack.png'
+// import jnvLogo from '../static/images/avatar/jnvLogo.png'
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import { Dialog, DialogTitle, DialogContent, DialogContentText } from '@mui/material'
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+
 
 
 
@@ -30,123 +33,145 @@ const Resume = () => {
                     <HorizontalRuleRoundedIcon fontSize='large' />
                 </Grid>
             </Grid>
-            <Grid sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', gap: 20 }}>
-                <Grid Item xs={6} sx={{ width: 400 }}>
-                    <Typography textAlign='center' sx={{ fontSize: 30, mb: 3, color: '#3AA6B9' }}>Work Experience</Typography>
-                    {/* <List sx={{ width: '100%', bgcolor: '#2f6163'}}> */}
-                    <List sx={{ width: '100%', bgcolor: 'transparent' }}>
-                        <ListItemButton onClick={() => setOpen(true)}>
-                            <ListItem>
-                                <ListItemAvatar>
-                                    <Avatar alt="Searce Inc" src={searceLogoBlack} sx={{ bgcolor: 'black', width: 45, height: 45 }} />
-                                </ListItemAvatar>
-                                <ListItemText>
-                                    <Typography sx={{ color: 'white' }}>
-                                        Software Engineer
-                                    </Typography>
-                                    <Typography sx={{ color: 'white', fontSize: 13 }}>
-                                        August, 22 - Present
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary" sx={{ color: '#00ff00', fontSize: 13 }}>
-                                        Python | FastAPI | Google Cloud Datastore | Django | Apigee | Cloud SQL | PandasAI
-                                    </Typography>
-                                </ListItemText>
-                            </ListItem>
-                        </ListItemButton>
-                        <Divider variant="middle" />
-                        <ListItemButton>
-                            <ListItem>
-                                <ListItemAvatar>
-                                    <Avatar alt="Searce Inc" src={searceLogoBlack} sx={{ bgcolor: 'black', width: 45, height: 45 }} />
-                                </ListItemAvatar>
-                                <ListItemText>
-                                    <Typography sx={{ color: 'white' }}>
-                                        Software Engineer Intern
-                                    </Typography>
-                                    <Typography sx={{ color: 'white', fontSize: 13 }}>
-                                        Feb, 22 - July, 22
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary" sx={{ color: '#00ff00', fontSize: 13 }}>
-                                        Python | FastAPI | Microsoft SQL Server | MySQL
-                                    </Typography>
-                                </ListItemText>
-                            </ListItem>
-                        </ListItemButton>
-                        <Divider variant="middle" />
-                        <ListItemButton>
-                            <ListItem>
-                                <ListItemAvatar>
-                                    <Avatar alt="Searce Inc" src={tejgyanLogo} sx={{ bgcolor: 'black', width: 45, height: 45 }} />
-                                </ListItemAvatar>
-                                <ListItemText>
-                                    <Typography sx={{ color: 'white' }}>
-                                        Project Intern
-                                    </Typography>
-                                    <Typography sx={{ color: 'white', fontSize: 13 }}>
-                                        July, 21 - December, 21
-                                    </Typography>
-                                    <Typography variant="body2" color="text.secondary" sx={{ color: '#00ff00', fontSize: 13 }}>
-                                        HTML | CSS | Python | Flask
-                                    </Typography>
-                                </ListItemText>
-                            </ListItem>
-                        </ListItemButton>
-                    </List>
+            <Grid sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mb: 5 }}>
+                <Grid Item xs={6}>
+                    <Typography textAlign='center' sx={{ fontSize: 30, color: '#3AA6B9' }}>Work Experience</Typography>
                 </Grid>
-                <Grid Item xs={6} sx={{ width: 400 }}>
-                    <Typography textAlign='center' sx={{ fontSize: 30, mb: 3, color: '#3AA6B9' }}>Education</Typography>
-                    <List sx={{ width: '100%', bgcolor: 'transparent' }}>
-                        <ListItem sx={{m:1}}>
-                            <ListItemAvatar>
-                                <Avatar alt="Searce Inc" src={viitLogo} sx={{ bgcolor: 'black', width: 45, height: 45 }} />
-                            </ListItemAvatar>
-                            <ListItemText>
-                                <Typography sx={{ color: 'white' }}>
-                                    Vishwakarma Institute of Information Technology, Pune
+                <Grid Item xs={6}>
+                    <Timeline>
+                        <TimelineItem>
+                            <TimelineOppositeContent>
+                                <Typography sx={{ color: 'white', fontSize: 15 }}>
+                                    August, 22 - Present
                                 </Typography>
-                                <Typography sx={{ color: 'white', fontSize: 13 }}>
+                            </TimelineOppositeContent>
+                            <TimelineSeparator>
+                                <TimelineDot />
+                                <TimelineConnector />
+                            </TimelineSeparator>
+                            <TimelineContent sx={{ mb: 4 }}>
+                                <Typography sx={{ color: 'white', fontSize: 20 }}>
+                                    Software Engineer
+                                </Typography>
+                                <Typography sx={{ color: 'white', fontSize: 15 }}>
+                                    Searce Inc.
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary" sx={{ color: '#00ff00', fontSize: 13 }}>
+                                    Python | FastAPI | Google Cloud Datastore | Django | Apigee | Cloud SQL | PandasAI
+                                </Typography>
+                            </TimelineContent>
+                        </TimelineItem>
+                        <TimelineItem>
+                            <TimelineOppositeContent>
+                                <Typography sx={{ color: 'white', fontSize: 15 }}>
+                                    Feb, 22 - July, 22
+                                </Typography>
+                            </TimelineOppositeContent>
+                            <TimelineSeparator>
+                                <TimelineDot />
+                                <TimelineConnector />
+                            </TimelineSeparator>
+                            <TimelineContent sx={{ mb: 4 }}>
+                                <Typography sx={{ color: 'white', fontSize: 20 }}>
+                                    Software Engineer Intern
+                                </Typography>
+                                <Typography sx={{ color: 'white', fontSize: 15 }}>
+                                    Searce Inc.
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary" sx={{ color: '#00ff00', fontSize: 13 }}>
+                                    Python | FastAPI | Microsoft SQL Server | MySQL
+                                </Typography>
+                            </TimelineContent>
+                        </TimelineItem>
+                        <TimelineItem>
+                            <TimelineOppositeContent>
+                                <Typography sx={{ color: 'white', fontSize: 15 }}>
+                                    July, 21 - December, 21
+                                </Typography>
+                            </TimelineOppositeContent>
+                            <TimelineSeparator>
+                                <TimelineDot />
+                                <TimelineConnector />
+                            </TimelineSeparator>
+                            <TimelineContent sx={{ mb: 4 }}>
+                                <Typography sx={{ color: 'white', fontSize: 20 }}>
+                                    Project Intern
+                                </Typography>
+                                <Typography sx={{ color: 'white', fontSize: 15 }}>
+                                    Tejgyan Foundation (TCS-TGF)
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary" sx={{ color: '#00ff00', fontSize: 13 }}>
+                                    HTML | CSS | Python | Flask
+                                </Typography>
+                            </TimelineContent>
+                        </TimelineItem>
+                    </Timeline>
+                </Grid>
+            </Grid>
+            <Grid sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <Grid Item xs={6}>
+                    <Typography textAlign='center' sx={{ fontSize: 30, color: '#3AA6B9' }}>Education</Typography>
+                </Grid>
+                <Grid Item xs={6} sx={{width: '100%'}}>
+                    <Timeline>
+                        <TimelineItem>
+                            <TimelineOppositeContent>
+                                <Typography sx={{ color: 'white', fontSize: 15 }}>
                                     2018 - 2022
                                 </Typography>
-                                <Typography sx={{ color: 'white', fontSize: 13 }}>
+                            </TimelineOppositeContent>
+                            <TimelineSeparator>
+                                <TimelineDot />
+                                <TimelineConnector />
+                            </TimelineSeparator>
+                            <TimelineContent sx={{ mb: 4 }}>
+                                <Typography sx={{ color: 'white', fontSize: 20 }}>
+                                    Vishwakarma Institute of Information Technology, Pune
+                                </Typography>
+                                <Typography sx={{ color: '#00ff00', fontSize: 15 }}>
                                     B. Tech - Information Technology | CGPA: 9.35
                                 </Typography>
-                            </ListItemText>
-                        </ListItem>
-                        <Divider variant="middle" />
-                        <ListItem sx={{ m: 1 }}>
-                            <ListItemAvatar>
-                                <Avatar alt="Searce Inc" src={narayanaLogoBlack} sx={{ bgcolor: 'black', width: 45, height: 45 }} />
-                            </ListItemAvatar>
-                            <ListItemText>
-                                <Typography sx={{ color: 'white' }}>
-                                    Narayana Junior College, Hyderabad
-                                </Typography>
-                                <Typography sx={{ color: 'white', fontSize: 13 }}>
+                            </TimelineContent>
+                        </TimelineItem>
+                        <TimelineItem>
+                            <TimelineOppositeContent>
+                                <Typography sx={{ color: 'white', fontSize: 15 }}>
                                     2016 - 2018
                                 </Typography>
-                                <Typography sx={{ color: 'white', fontSize: 13 }}>
+                            </TimelineOppositeContent>
+                            <TimelineSeparator>
+                                <TimelineDot />
+                                <TimelineConnector />
+                            </TimelineSeparator>
+                            <TimelineContent sx={{ mb: 4 }}>
+                                <Typography sx={{ color: 'white', fontSize: 20 }}>
+                                    Narayana Junior College, Hyderabad
+                                </Typography>
+                                <Typography sx={{ color: '#00ff00', fontSize: 15 }}>
                                     HSC (Intermediate) - PCM | Marks (in %): 97.2
                                 </Typography>
-                            </ListItemText>
-                        </ListItem>
-                        <Divider variant="middle" />
-                        <ListItem sx={{ m: 1 }}>
-                            <ListItemAvatar>
-                                <Avatar alt="Searce Inc" src={jnvLogo} sx={{ bgcolor: 'black', width: 45, height: 45 }} />
-                            </ListItemAvatar>
-                            <ListItemText>
-                                <Typography sx={{ color: 'white' }}>
-                                    Jawahar Navodaya Vidyalaya, Parbhani
-                                </Typography>
-                                <Typography sx={{ color: 'white', fontSize: 13 }}>
+                            </TimelineContent>
+                        </TimelineItem>
+                        <TimelineItem>
+                            <TimelineOppositeContent>
+                                <Typography sx={{ color: 'white', fontSize: 15 }}>
                                     2015 - 2016
                                 </Typography>
-                                <Typography sx={{ color: 'white', fontSize: 13 }}>
+                            </TimelineOppositeContent>
+                            <TimelineSeparator>
+                                <TimelineDot />
+                                <TimelineConnector />
+                            </TimelineSeparator>
+                            <TimelineContent sx={{ mb: 4 }}>
+                                <Typography sx={{ color: 'white', fontSize: 20 }}>
+                                    Jawahar Navodaya Vidyalaya, Parbhani
+                                </Typography>
+                                <Typography sx={{ color: '#00ff00', fontSize: 15 }}>
                                     SSC (CBSE) | Marks (in %): 94.2
                                 </Typography>
-                            </ListItemText>
-                        </ListItem>
-                    </List>
+                            </TimelineContent>
+                        </TimelineItem>
+                    </Timeline>
                 </Grid>
             </Grid>
             <Dialog open={open} onClose={() => setOpen(false)}>
@@ -158,7 +183,7 @@ const Resume = () => {
                     </DialogContentText>
                     <DialogContentText sx={{ display: 'flex', flexDirection: 'row' }}>
                         <KeyboardArrowRightIcon />
-                        Currently working on a product that streamlines the enterprise's document processing lifecycle.                    
+                        Currently working on a product that streamlines the enterprise's document processing lifecycle.
                     </DialogContentText>
                     <DialogContentText sx={{ display: 'flex', flexDirection: 'row' }}>
                         <KeyboardArrowRightIcon />
@@ -170,7 +195,7 @@ const Resume = () => {
                     </DialogContentText>
                     <DialogContentText sx={{ display: 'flex', flexDirection: 'row' }}>
                         <KeyboardArrowRightIcon />
-                        Technologies used: Python, FastAPI, Google Cloud Datastore, Django, Apigee, Cloud SQL, PandasAI                    
+                        Technologies used: Python, FastAPI, Google Cloud Datastore, Django, Apigee, Cloud SQL, PandasAI
                     </DialogContentText>
                 </DialogContent>
             </Dialog>
